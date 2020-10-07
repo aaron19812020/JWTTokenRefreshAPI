1132f6cbd0fbcb3c37ac1e87a4bd1ad04db7662d
aaronlin <codegear2018@hotmail.com>
2020-10-07 15:17:13 +0800

    添加JWTTokenRefresh业务应用API

---
+++ FlyLolo.JWT.API/Controllers/BookController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlyLolo.JWT.API.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    //public class BookController : ControllerBase
+    //{
+
+    //}
+
+
+    [Route("api/[controller]")]
+    [Authorize]
+    public class BookController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        [AllowAnonymous]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "ASP", "C#" };
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public JsonResult Post()
+        {
+            return new JsonResult("Create  Book ...");
+        }
+    }
+}
---
+++ FlyLolo.JWT.API/Models/TokenValidationParameters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyLolo.JWT.API.Models
+{
+    //public class TokenValidationParameters
+    //{
+    //    public TokenValidationParameters()
+    //    {
+    //        RequireExpirationTime = true;
+    //        RequireSignedTokens = true;
+    //        SaveSigninToken = false;
+    //        ValidateActor = false;
+    //        ValidateAudience = true;  //是否验证接受者
+    //        ValidateIssuer = true;   //是否验证发布者
+    //        ValidateIssuerSigningKey = false;  //是否验证秘钥
+    //        ValidateLifetime = true; //是否验证过期时间
+    //        ValidateTokenReplay = false;
+    //    }
+    //}
+}
